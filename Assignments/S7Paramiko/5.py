3acca03803cb62d2b82826974cbd18c6bbc7b8e3
Jeremy Thomas <108291743+JaayyTeeGithub@users.noreply.github.com>
2022-09-13 19:33:25 +0300

    update to docstring for 5.py
    Create a Python script that connects to a Cisco Router using SSH and Paramiko, enters the enable mode, and then executes show running-config
    
    The entire output should be saved to a file in the current working directory.

--- Assignments/S7Paramiko/5.py
+++ Assignments/S7Paramiko/5.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+'''
+Create a Python script that connects to a Cisco Router using SSH and Paramiko, 
+enters the enable mode, and then executes show running-config
+
+The entire output should be saved to a file in the current working directory.
+'''
 
 import paramiko
 import getpass
