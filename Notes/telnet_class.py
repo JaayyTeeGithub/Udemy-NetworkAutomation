1ec867106840d8d2b26f75a52e8c96dec699111b
Jeremy Thomas <108291743+JaayyTeeGithub@users.noreply.github.com>
2022-09-27 11:41:48 -0500

    Update telnet_class.py

--- Notes/telnet_class.py
+++ Notes/telnet_class.py
@@ -26,3 +26,21 @@ class Device:
     def show(self):
         output = self.tn.read_all().decode('utf-8')
         return output
+    
+router = Device('10.1.1.20', 'u1', 'cisco')
+router.connect()
+router.authenticate()
+router.send('enable')
+router.send('cisco')
+router.send('conf t')
+router.send('int loopback 0')
+router.send(f'ip address 1.1.1.1 255.255.255.255')
+router.send('exit')
+router.send('router ospf 1')
+router.send('net 0.0.0.0 0.0.0.0 area 0')
+router.send('end')
+router.send('terminal length 0')
+router.send('show ip protocols')
+router.send('exit')
+output = router.show()
+print(output)
